d27fc747c89e8adcacddd17fe558a77cd6a6f55f
sanathsharma1 <77628384+sanathsharma1@users.noreply.github.com>
2024-05-06 17:10:29 +0530

    disable mouse

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,6 +3,7 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set tabstop=2")
 vim.opt.relativenumber = true
+vim.opt.mouse = ""
 vim.opt.number = true
 vim.opt.foldmethod = "manual"
 vim.g.mapleader = " "
