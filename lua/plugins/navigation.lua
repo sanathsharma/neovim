f738fae334043c24fd4154c41defe1864c7e621d
sanathsharma <77628384+sanathsharma1@users.noreply.github.com>
2024-05-09 11:10:05 +0530

    feat(navigation): add oil.nvim for better file navigation
    - keep neo-tree configuration as is, as it would help it some scenarios
    - move neo-tree configuration into navigation.lua file and add oil.nvim
      configuration in the same file along with harpoon and tmux navigator
      as they are all related

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -50,8 +50,59 @@ return {
 			for i = 1, 4, 1 do
 				vim.keymap.set("n", "<leader>m" .. i, function()
 					ui.nav_file(i)
-					end, { desc = "Jump to mark: " .. i })
+				end, { desc = "Jump to mark: " .. i })
 			end
+		end,
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			vim.keymap.set("n", "<C-n>", ":Neotree source=filesystem toggle position=left reveal<CR>")
+
+			require("neo-tree").setup({
+				filesystem = {
+					follow_current_file = {
+						enabled = true,
+						leave_dirs_open = false,
+					},
+				},
+			})
+		end,
+	},
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("oil").setup({
+				default_file_explorer = true,
+				keymaps = {
+					["g?"] = "actions.show_help",
+					["<CR>"] = "actions.select",
+					["<C-s>"] = "actions.select_vsplit",
+					["<C-h>"] = "actions.select_split",
+					["<C-t>"] = "actions.select_tab",
+					["<C-p>"] = "actions.preview",
+					["<C-c>"] = "actions.close",
+					["<C-l>"] = "actions.refresh",
+					["-"] = "actions.parent",
+					["_"] = "actions.open_cwd",
+					["`"] = "actions.cd",
+					["~"] = "actions.tcd",
+					["gs"] = "actions.change_sort",
+					["gx"] = "actions.open_external",
+					["g."] = "actions.toggle_hidden",
+					["g\\"] = "actions.toggle_trash",
+				},
+			})
+
+			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 		end,
 	},
 }
