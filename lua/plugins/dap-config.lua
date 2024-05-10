0622e315eb0c04439b391bb09a31bc95778bdaaa
sanathsharma <77628384+sanathsharma1@users.noreply.github.com>
2024-05-10 09:58:16 +0530

    feat(dap): add dap support for js,ts,tsx,jsx and go
    - go debug support with nvim-dap-go plugin
    - nodejs server debug support with js-dubug-adaptor (microsoft/vscode)
    - react, chrome debug support with chrome-debug-adaptor
    (micrsoft/vscode)

--- lua/plugins/dap-config.lua
+++ lua/plugins/dap-config.lua
@@ -1,12 +1,120 @@
+local js_based_languages = {
+	"typescript",
+	"javascript",
+	"typescriptreact",
+	"javascriptreact",
+	"vue",
+}
+
 return {
 	{
-		"rcarriga/nvim-dap-ui",
+		"mfussenegger/nvim-dap",
 		dependencies = {
-			"mfussenegger/nvim-dap",
+			"rcarriga/nvim-dap-ui",
 			"nvim-neotest/nvim-nio",
+			"leoluz/nvim-dap-go",
 		},
 		config = function()
-			require("dapui").setup()
+			local dap, dapui = require("dap"), require("dapui")
+
+			require("dap-go").setup()
+			dapui.setup()
+
+			dap.adapters["pwa-node"] = {
+				type = "server",
+				host = "127.0.0.1",
+				port = 8123,
+				executable = {
+					command = "js-debug-adapter",
+				},
+			}
+
+			dap.adapters["chrome"] = {
+				type = "executable",
+				command = "chrome-debug-adapter",
+				args = { "--remote-debugging-port=9222" },
+			}
+
+			for _, language in ipairs(js_based_languages) do
+				dap.configurations[language] = {
+					-- Debug single nodejs files
+					{
+						type = "pwa-node",
+						request = "launch",
+						name = "Launch file",
+						program = "${file}",
+						cwd = vim.fn.getcwd(),
+						sourceMaps = true,
+					},
+					-- Debug nodejs processes (make sure to add --inspect when you run the process)
+					{
+						type = "pwa-node",
+						request = "attach",
+						name = "Attach",
+						processId = require("dap.utils").pick_process,
+						cwd = vim.fn.getcwd(),
+						sourceMaps = true,
+					},
+					-- Debug web applications (client side)
+					{
+						type = "chrome",
+						request = "launch",
+						name = "Launch & Debug Chrome",
+						url = function()
+							local co = coroutine.running()
+							return coroutine.create(function()
+								vim.ui.input({
+									prompt = "Enter URL: ",
+									default = "http://localhost:3000",
+								}, function(url)
+									if url == nil or url == "" then
+										return
+									else
+										coroutine.resume(co, url)
+									end
+								end)
+							end)
+						end,
+						webRoot = vim.fn.getcwd(),
+						protocol = "inspector",
+						sourceMaps = true,
+						userDataDir = false,
+					},
+					{
+						type = "chrome",
+						request = "attach",
+						name = "Attach to chrome",
+						program = "${file}",
+						processId = require("dap.utils").pick_process,
+						cwd = vim.fn.getcwd(),
+						sourceMaps = true,
+						protocol = "inspector",
+						port = 9222,
+						webRoot = "${workspaceFolder}",
+					},
+
+					-- Divider for the launch.json derived configs
+					{
+						name = "----- ↓ launch.json configs ↓ -----",
+						type = "",
+						request = "launch",
+					},
+				}
+			end
+
+			-- event listeners
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open({ reset = true })
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open({ reset = true })
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
 
 			-- keymaps
 			vim.keymap.set(
